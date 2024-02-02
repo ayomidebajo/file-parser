7d792f26ccb669d5bb147ea38ab3ce8aa7770f5d
Ayomide Bajo <oluwashinabajo@gmail.com>
2024-02-02 16:38:30 +0100

    change cli configuration to use Clap instead

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,37 @@
+use clap::Parser;
 use std::env;
 pub mod file_types;
 
-fn main() {
+#[derive(Parser, Debug)]
+#[command(author, version, about)]
+struct Cli {
+    #[arg(short, long)]
+    file_name: String,
+    #[arg(short, long)]
+    ext_name: String,
+}
 
-    let args: Vec<String> = env::args().skip(1).collect();    
-    
-    let file_name = args[0].clone();
-    let file_type: Vec<&str>= file_name.split(".").collect();
+fn main() {
+    let args = Cli::parse();
+
+    let file_name = args.file_name;
+    let file_type: Vec<&str> = file_name.split(".").collect();
 
     // a simple error and input sanitizer
     if file_type.len() > 2 {
         println!("incorrect file type!");
-        return
+        return;
     }
 
     match file_type[1] {
-       "txt" => {
-        println!("This is a txt file")
-       }, 
-       _ => {
-        println!("Unsupported file type")
-       }
+        "txt" => {
+            println!("This is a txt file")
+        }
+        _ => {
+            println!("Unsupported file type")
+        }
     }
 
     // todo: add path buf and experiment reading a file buffer from a format and write in another format.
     // make more research on paths
-
 }
